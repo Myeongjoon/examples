28054ae6eb8403f9b543957209a702ca62b9419a
myeongjoon_kim <myeongjoon_kim@tmax.co.kr>
2021-03-29 12:41:48 +0900

    MODIFY B

--- problem solving/code jam/2021/qualification/B.java
+++ problem solving/code jam/2021/qualification/B.java
@@ -5,23 +5,24 @@ class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int t = scanner.nextInt();
+        scanner.nextLine();
         for (int tt = 1; tt <= t; tt++) {
-            int a = scanner.nextInt();
-            int b = scanner.nextInt();
             String s = scanner.nextLine();
-            int[][] dp = new int[s.length()][2];
-            dp[0][0] = 0;
-            dp[0][1] = 0;
-            for (int i = 1; i < s.length(); i++) {
+            String[] sliced = s.split(" ");
+            int a = Integer.parseInt(sliced[0]);
+            int b = Integer.parseInt(sliced[1]);
+            s = sliced[2];
+            long[][] dp = new long[s.length()][2];
+            for (int i = 0; i < s.length(); i++) {
                 if (s.charAt(i) == 'C') {
-                    dp[i][1] = 100*1000;
-                    dp[i][0] = Math.min(dp[i - 1][1] + b, dp[i - 1][0]);
+                    dp[i][1] = Integer.MAX_VALUE;
+                    dp[i][0] = i == 0 ? 0 : Math.min(dp[i - 1][1] + b, dp[i - 1][0]);
                 } else if (s.charAt(i) == 'J') {
-                    dp[i][0] = 100*1000;
-                    dp[i][1] = Math.min(dp[i - 1][0] + a, dp[i - 1][1]);
-                } else {
-                    dp[i][1] = Math.min(dp[i - 1][0] + a, dp[i - 1][1]);
-                    dp[i][0] = Math.min(dp[i - 1][1] + b, dp[i - 1][0]);
+                    dp[i][0] = Integer.MAX_VALUE;
+                    dp[i][1] = i == 0 ? 0 : Math.min(dp[i - 1][0] + a, dp[i - 1][1]);
+                } else if (s.charAt(i) == '?') {
+                    dp[i][1] = i == 0 ? 0 : Math.min(dp[i - 1][0] + a, dp[i - 1][1]);
+                    dp[i][0] = i == 0 ? 0 : Math.min(dp[i - 1][1] + b, dp[i - 1][0]);
                 }
             }
             long min = Math.min(dp[s.length() - 1][0], dp[s.length() - 1][1]);
